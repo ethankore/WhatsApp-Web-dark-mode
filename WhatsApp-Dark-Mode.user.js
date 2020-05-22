49f42243dd7bd1570d69d6e2b419fcf146e7d1fd
Tal Koren <ardethian@gmail.com>
2020-05-22 12:52:52 +0300

    updating some deets

--- WhatsApp-Dark-Mode.user.js
+++ WhatsApp-Dark-Mode.user.js
@@ -3,7 +3,7 @@
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  Enabling official dark mode in WhatsApp Web
-// @author       You
+// @author       Tal Koren - https://github.com/tkore/
 // @match        https://web.whatsapp.com/
 // @grant        none
 // @updateURL    https://github.com/tkore/WhatsApp-Web-dark-mode/raw/master/WhatsApp-Dark-Mode.user.js
